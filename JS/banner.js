929f2f6ab2e979d95a56dbd67fd276690cf58892
Sombit Pramanik <m.sombitpramanik@gmail.com>
2024-02-13 16:47:25 +0530

    add the secund banner

--- JS/banner.js
+++ JS/banner.js
@@ -27,3 +27,28 @@ function nextSlide() {
 
 // Automatically switch to next slide every 20 seconds
 setInterval(nextSlide, 20000);
+
+let banner2 = document.getElementsByClassName("bannerItem2");
+banner2[0].style.backgroundImage = "url('Assets/BANNER IMAGES/banner2/img1.jpg')";
+banner2[1].style.backgroundImage = "url('Assets/BANNER IMAGES/banner2/img2.jpg')";
+banner2[2].style.backgroundImage = "url('Assets/BANNER IMAGES/banner2/img3.jpg')";
+banner2[3].style.backgroundImage = "url('Assets/BANNER IMAGES/banner2/img4.jpg')";
+let banner2CurrentSlide = 0;
+
+b2showSlide(banner2CurrentSlide)
+
+function b2showSlide(slideIndex) {
+    // Hide all slides
+    for (let i = 0; i < banner2.length; i++) {
+        banner2[i].classList.remove("active");
+    }
+
+    // Show the current slide
+    banner2[slideIndex].classList.add("active");
+}
+
+function banner2NextSlide(){
+    banner2CurrentSlide = (banner2CurrentSlide +1) % banner2.length;
+    b2showSlide(banner2CurrentSlide);
+}
+setInterval(banner2NextSlide,10000);
